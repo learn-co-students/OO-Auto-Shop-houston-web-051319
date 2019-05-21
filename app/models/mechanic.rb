42bf1adb91d5cbb0d3f9f6a1fa3e3e47c11acf0e
Brendan Brown <pbbrown1112@gmail.com>
2019-05-21 15:40:42 -0500

    Now the mechanics know their clients

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,10 +1,35 @@
 class Mechanic
 
-  attr_reader :name, :specialty
+attr_reader :name, :specialty
+
+@@all = []
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def cars
+  Car.all.select do | car |
+    car.classification.downcase == self.specialty.downcase
+  end
+end
+
+def car_owners
+  self.cars.map do | car |
+    car.car_owner
+  end
+end
+
+def car_owner_names
+  self.car_owners.map do | car_owners |
+    car_owners.name
+  end
 end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,32 @@ def reload
   load 'config/environment.rb'
 end
 
+# Mechanics
+
+raul = Mechanic.new("Raul Sánchez", "SUV")
+hans = Mechanic.new("Hans Krohn", "Sports Car")
+roy = Mechanic.new("Royson Easo", "Minivan")
+maruf = Mechanic.new("Maruf Awal", "Economy")
+
+# CarOwners
+
+stefan = CarOwner.new("Stefan Stojanovic")
+hermann = CarOwner.new("Hermann Krohn")
+kurtis = CarOwner.new("Kurtis Leach")
+feven = CarOwner.new("Feven Tsegay")
+
+# Cars
+
+car1 = Car.new("BMW", "M3", "Sports Car", hans, stefan)
+car2 = Car.new("VW", "Toureg", "SUV", raul, hermann)
+car3 = Car.new("Porsche", "GT2", "Sports Car", hans, kurtis)
+car4 = Car.new("Audi", "Q5", "SUV", raul, feven)
+car5 = Car.new("Honda", "Odyssey", "Minivan", roy, stefan)
+car6 = Car.new("Audi", "RS5", "Sports Car", hans, hermann)
+car7 = Car.new("Honda", "Civic", "Economy", maruf, hermann)
+
+
+
+
+
 binding.pry
