904ada46311ac0125757533901db6d412893a615
Hans Krohn <hanskrohn70@gmail.com>
2019-05-21 15:19:51 -0500

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,31 @@
 class Mechanic
-
+  @@all = [] 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def car
+    #all the car objects a mechanic takes care of
+    Car.all.select do |car_object|
+      car_object.mechanic == self
+    end
+  end
+  def car_owner
+    self.car.map do |car_object|
+      car_object.car_owner
+    end
+  end
+  def car_owner_name
+    self.car_owner.map do |car_owner|
+      car_owner.name
+    end
+  end
 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,46 @@ def reload
   load 'config/environment.rb'
 end
 
+mechanic = Mechanic.new("Mechanic","Sport Car")
+mechanic2 = Mechanic.new("Mechanic2","Trucks")
+mechanic3 = Mechanic.new("Mechanic3","Simple")
+mechanic4 = Mechanic.new("Mechanic4","Sport Car")
+
+car_owner = CarOwner.new("Owner")
+car_owner2 = CarOwner.new("Owner2")
+car_owner3 = CarOwner.new("Owner3")
+
+sport_car = Car.new(car_owner, mechanic, "1","1","Sport Car")
+sport_car2 = Car.new(car_owner2, mechanic, "1","1","Truck")
+sport_car3 = Car.new(car_owner2, mechanic2, "2","2","Simple")
+sport_car4 = Car.new(car_owner2, mechanic2, "2","2","Simple")
+
 binding.pry
+0
+# CarOwner
+
+# Get a list of all owners
+
+# Get a list of all the cars that a specific owner has
+
+# Get a list of all the mechanics that a specific owner goes to
+
+# Get the average amount of cars owned for all owners
+
+# Car
+
+# Get a list of all cars
+
+# Get a list of all car classifications
+
+# Get a list of mechanics that have an expertise that matches the car classification
+
+# Mechanic
+
+# Get a list of all mechanics
+
+# Get a list of all cars that a mechanic services
+
+# Get a list of all the car owners that go to a specific mechanic
+
+# Get a list of the names of all car owners who go to a specific mechanic
